99e67bb9617a0614de0cb8387be155cd1f4e44d8
Sandip Kumar Dey <96774612+sandipkumardey@users.noreply.github.com>
2024-11-23 14:59:20 +0530

    added basic BASH scripts

---
+++ BASH Scripts/bestdayever.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+name="Jack"
+
+echo "Good Morning $name!!"
+
+sleep 3
+
+echo "You're Looking good today $name!!"
+
+sleep 3
+
+echo "You have the best beard I've ever seen $name!!"
+
+
+
+
+
