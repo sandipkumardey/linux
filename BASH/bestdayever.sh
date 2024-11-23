371eeb8dcef2c36634d9cec760f6a3dc06da5454
Sandip Kumar Dey <96774612+sandipkumardey@users.noreply.github.com>
2024-11-23 17:50:11 +0530

    added variables in bestdayever script

---
+++ BASH/bestdayever.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+name=$1
+compliment=$2
+
+user=$(whoami)
+date=$(date)
+whereami=$(pwd)
+
+echo "Good Morning $name!!"
+sleep 1
+echo "You're Looking good today $name!!"
+sleep 1
+echo "You have the best $compliment I've ever seen $name!!"
+sleep 3
+
+echo "You are currently logged in as $user and you are in the directory $whereami. Also today is: $date"
+
+
+
+
